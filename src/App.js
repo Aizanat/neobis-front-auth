baebc736c27a05c6611cafddc2351539596516e6
Aizanat <anukieva@mail.ru>
2024-05-08 13:41:46 +0400

    created the registration page with styles

--- src/App.js
+++ src/App.js
@@ -3,12 +3,14 @@ import ExitModal from './components/exitModal/ExitModal'
 import Welcome from './components/welcome/Welcome'
 import RegLetter from './components/registerLetter/RegLetter'
 import Auth from './pages/authorization/Auth'
+import Register from './pages/registration/Register'
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<Auth />} />
       <Route path="/welcome" element={<Welcome />} />
+      <Route path="/registration" element={<Register />} />
     </Routes>
   )
 }
